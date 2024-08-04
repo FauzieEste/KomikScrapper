9456f6dce6d071b18e2e72b4ce11c274eb03079f
FauzieEste <162405484+FauzieEste@users.noreply.github.com>
2024-08-04 10:07:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,10 +11,7 @@ app.use(morgan("dev"));
 
 app.use(express.json());
 
-app.use("/", (req, res) => {
-  res.json({
-    message: "Welcome to the KomikScrapper API",
-});
+app.get('/', (_, res) => res.status(200).json({ status: 'Ok', message: 'Otakudesu unofficial API, made by rzkfyn with <3' }));
 /// Use Komikcast route
 app.use("/api/komikcast/", require("./routes/manga-route.js"));
 
